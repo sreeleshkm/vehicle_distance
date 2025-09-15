64315e24be3f853525d22122cd57e0ed3f565ced
sreeleshkm <sreelesh.murali@trenser.com>
2025-09-15 15:37:49 +0530

    Handled multiple face detection

--- distanceMeasure.py
+++ distanceMeasure.py
@@ -6,7 +6,7 @@ from time import sleep
 
 cascPath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascPath)
-log.basicConfig(filename='webcam.log',level=log.INFO)
+log.basicConfig(filename='webcam.log', level=log.INFO)
 
 video_capture = cv2.VideoCapture(0)
 width, height = int(video_capture.get(3)), int(video_capture.get(4))
@@ -19,7 +19,7 @@ known_width1 = 48
 known_distance2 = 2.2
 known_width2 = 107
 
-focalLength = known_distance1*known_width1
+focalLength = known_distance1 * known_width1
 
 while True:
     # Capture frame-by-frame
@@ -34,22 +34,27 @@ while True:
         minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
+    # Draw a rectangle around the faces and put distance text below each face
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        # Calculate distance for each face
+        distance = focalLength / w
+
+        # Put distance text below the rectangle box
+        text_position = (x, y + h + 20)
+        cv2.putText(frame, "%.2fM" % distance,
+                    text_position,
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.7, (0, 255, 0), 2)
 
     if anterior != len(faces):
         anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
-    if len(faces) > 0:
-        cv2.putText(frame, "%.2fM" % (focalLength/faces[0][2]),
-            (frame.shape[1] - 200, frame.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
-            2.0, (0, 255, 0), 2)
+        log.info("faces: " + str(len(faces)) + " at " + str(dt.datetime.now()))
 
     # Display the resulting frame
     out.write(frame)
     cv2.imshow('Video', frame)
-
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
